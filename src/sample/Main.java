0be059f452a8f378ff6248e6930111e2f717416c
Aransolis <18030151@itcelaya.edu.mx>
2020-10-08 19:31:36 -0500

    Agregga taquimecanografo

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,33 +2,112 @@ package sample;
 
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.ToolBar;
+import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import sample.ui.Memorama;
+import sample.ui.Taquimecanografo;
 
-import javax.xml.soap.Text;
 
-public class Main extends Application {
+public class Main extends Application implements EventHandler {
+    private VBox vPrincipal;
+
+    private MenuBar mnbPrincipal;
+    private Menu menCompetencia1, menCompetencia2, menSalir;
+    private MenuItem itmMemorama, itmPractica2, itmTerminar;
+    private ToolBar tlbMenu;
+    private Scene escena;
+    private Button btnToolbar1;
 
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
-        primaryStage.setTitle("Practicas de Topicos 2020");
+        crearUI();
+        primaryStage.setTitle("Prácticas de Tópicos 2020");
         primaryStage.setMaximized(true);
-       // primaryStage.setScene(new Scene(hbox, 500, 500));
+        primaryStage.setScene(escena);
+        primaryStage.addEventHandler(WindowEvent.WINDOW_HIDDEN, this);
         primaryStage.show();
 
-        new Memorama();
+
+    }
+
+    private void crearUI() {
+
+        mnbPrincipal = new MenuBar();
+
+        //Creación de los menus
+        menCompetencia1 = new Menu("Competencia 1");
+        menCompetencia2 = new Menu("Competencia 2");
+        menSalir = new Menu("Salir");
+
+        //Cargar los menus a la barra de menus
+        mnbPrincipal.getMenus().addAll(menCompetencia1, menCompetencia2, menSalir);
+
+        //Creamos el menuItem para el memorama de la primer competencia
+        itmMemorama = new MenuItem("Memorama");
+        itmMemorama.setOnAction(event -> opcionMenu(1));
+
+        itmPractica2 = new MenuItem("Taquimecanografo");
+        itmPractica2.setOnAction(event -> opcionMenu(2));
+
+        itmTerminar = new MenuItem("Hasta pronto :)");
+        itmTerminar.setOnAction(event -> {System.exit(0); });
+
+        //Cargar el item Memorama al menu "competencia1"
+        menCompetencia1.getItems().addAll(itmMemorama, itmPractica2);
+        menSalir.getItems().add(itmTerminar);
+
+        //Crear una barra de herramientas
+        tlbMenu = new ToolBar();
+        btnToolbar1 = new Button();
+        btnToolbar1.setOnAction(event -> opcionMenu(1));
+        btnToolbar1.setPrefSize(35, 35);
+
+        //Asignamos la imagen al boton dentro del toolbar
+        Image img = new Image("sample/assets/game.png");
+        ImageView imv = new ImageView(img);
+        imv.setFitHeight(35);
+        imv.setPreserveRatio(true);
+        btnToolbar1.setGraphic(imv);
+
+        tlbMenu.getItems().add(btnToolbar1);
+        vPrincipal = new VBox();
+        vPrincipal.getChildren().addAll(mnbPrincipal,tlbMenu);
+
+        escena = new Scene(vPrincipal);
+    }
+
+    private void opcionMenu(int opc) {
+        switch (opc) {
+            case 1:
+                new Memorama();
+                break;
+            case 2:
+                new Taquimecanografo();
+                break;
+        }
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void handle(Event event) {
+
+        System.out.println("Se esta mostrando la pantalla");
     }
 }
--- src/sample/ui/Memorama.java
+++ src/sample/ui/Memorama.java
@@ -6,6 +6,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -19,6 +21,8 @@ import javax.swing.*;
 
 public class Memorama extends Stage implements EventHandler {
 
+    private String[] arImagenes = {"horse.png", "lion.jpg", "monkey.png", "pajaro.png", "pig.png", "tigre.jpg", "turtle.png"};
+
     private Label lblTarjetas;
     private TextField txtNoTarjetas;
     private Button btnAceptar;
@@ -27,11 +31,12 @@ public class Memorama extends Stage implements EventHandler {
     private GridPane gdpMesa;
 
     private Button[][] arTarjetas;        //se puede realizar arreglo de botones
+    private String[][] arAsignacion;
 
     private Scene escena;
     private int noPares;
 
-    public Memorama(){
+    public Memorama() {
         CrearUI();
         this.setTitle("Memorama : ");
         this.setScene(escena);
@@ -42,8 +47,8 @@ public class Memorama extends Stage implements EventHandler {
         lblTarjetas = new Label("Numero de Tarjetas");
         txtNoTarjetas = new TextField();
         btnAceptar = new Button("Voltear y revolver");
-      btnAceptar.addEventHandler(MouseEvent.MOUSE_CLICKED, this);
-      //----Para mandar diferentes mensajes opcion 2 en una clase diferente mandando un argumento int----
+        btnAceptar.addEventHandler(MouseEvent.MOUSE_CLICKED, this);
+        //----Para mandar diferentes mensajes opcion 2 en una clase diferente mandando un argumento int----
 
       /*btnAceptar.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventosMemorama()); // el evento es llamado de otro paquete/clase
         btnAceptar.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
@@ -54,14 +59,14 @@ public class Memorama extends Stage implements EventHandler {
         });
 */
         hBox = new HBox();
-        hBox.getChildren().addAll(lblTarjetas, txtNoTarjetas,btnAceptar);
+        hBox.getChildren().addAll(lblTarjetas, txtNoTarjetas, btnAceptar);
         hBox.setSpacing(10);
 
         gdpMesa = new GridPane();
         vBox = new VBox();
-        vBox.getChildren().addAll(hBox,gdpMesa);
+        vBox.getChildren().addAll(hBox, gdpMesa);
 
-        escena = new Scene(vBox,500,500);
+        escena = new Scene(vBox, 500, 500);
     }
 
     @Override
@@ -70,13 +75,59 @@ public class Memorama extends Stage implements EventHandler {
         vBox.getChildren().remove(gdpMesa);
 
         gdpMesa = new GridPane();
+        arAsignacion = new String[2][noPares];
+        revolver();
         arTarjetas = new Button[2][noPares];
-        for (int i = 0; i<2; i++){
-            for(int j =0; j<noPares; j++){
-                arTarjetas[i][j] = new Button("X");
-                gdpMesa.add(arTarjetas[i][j],j,i);
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < noPares; j++) {
+
+                Image img = new Image("sample/assets/question.jpg");
+                ImageView imv = new ImageView(img);
+                imv.setFitHeight(100);
+                imv.setPreserveRatio(true);
+
+                arTarjetas[i][j] = new Button();
+                int finalI = i;
+                int finalJ = j;
+                arTarjetas[i][j].setOnAction(event1 -> verTarjeta(finalI, finalJ));
+                arTarjetas[i][j].setGraphic(imv);
+                arTarjetas[i][j].setPrefSize(80, 120);
+
+                gdpMesa.add(arTarjetas[i][j], j, i);
             }
         }
         vBox.getChildren().add(gdpMesa);
+    }
+
+    private void verTarjeta(int finalI, int finalJ) {
+        Image img = new Image("sample/assets/"+arAsignacion[finalI][finalJ]);
+        ImageView imv = new ImageView(img);
+        imv.setFitHeight(80);
+        imv.setPreserveRatio(true);
+
+        arTarjetas[finalI][finalJ].setGraphic(imv);
+    }
+
+    private void revolver() {
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < noPares; j++) {
+                arAsignacion[i][j] = new String();
+            }
+
+        int posx, posy, cont = 0;
+        for (int i = 0; i < noPares; ) {
+            posx = (int) (Math.random() * 2);
+            posy = (int) (Math.random() * noPares);
+            if (arAsignacion[posx][posy].equals("")) {
+                arAsignacion[posx][posy] = arImagenes[i];
+                cont++;
+            }
+
+            if (cont == 2) { // Sirve para comprobar que la imagen se asignó 2 veces
+                i++;
+                cont = 0;
+            }
+        }
     }
 }
